58003b53678911a32a05c8a8b26ab6a9ace37575
johandiepstraten <johandiepstraten@gmail.com>
2018-11-09 13:10:11 +0100

    Final Project

--- app/src/main/java/com/example/johan/tic_tac_toe/Game.java
+++ app/src/main/java/com/example/johan/tic_tac_toe/Game.java
@@ -1,6 +1,11 @@
 package com.example.johan.tic_tac_toe;
 
-public class Game {
+import android.view.View;
+import android.widget.Button;
+
+import java.io.Serializable;
+
+public class Game implements Serializable {
     final private int BOARD_SIZE = 3;
     private TileState[][] board;
 
@@ -8,6 +13,7 @@ public class Game {
     private int movesPlayed;
     private Boolean gameOver;
 
+    // Setup game board
     public Game() {
         board = new TileState[BOARD_SIZE][BOARD_SIZE];
         for(int i=0; i<BOARD_SIZE; i++)
@@ -17,9 +23,11 @@ public class Game {
         playerOneTurn = true;
         gameOver = false;
     }
+
+    // change selected tile to cross or circle in blank
     public TileState choose(int row, int column) {
         if(board[row][column] == TileState.BLANK)   {
-            if(playerOneTurn == true)   {
+            if(playerOneTurn)   {
                 board[row][column] = TileState.CROSS;
                 playerOneTurn = false;
                 return TileState.CROSS;
@@ -32,5 +40,37 @@ public class Game {
             return TileState.INVALID;
         }
 
+    }
+
+    // Check if game is won
+    public GameState won()   {
+        if((board[0][0] == TileState.CROSS && board[1][1] == TileState.CROSS &&
+                board[2][2] == TileState.CROSS) || (board[0][2] == TileState.CROSS &&
+                board[1][1] == TileState.CROSS && board[2][0] == TileState.CROSS))  {
+            return GameState.PLAYER_ONE;
+        }
+        for(int i=0; i<BOARD_SIZE; i++)
+            if((board[i][0] == TileState.CROSS && board[i][1] == TileState.CROSS &&
+                    board[i][2] == TileState.CROSS) || (board[0][i] == TileState.CROSS &&
+                    board[1][i] == TileState.CROSS && board[2][i] == TileState.CROSS))  {
+                return GameState.PLAYER_ONE;
+            }
+        if((board[0][0] == TileState.CIRCLE && board[1][1] == TileState.CIRCLE &&
+                board[2][2] == TileState.CIRCLE) || (board[0][2] == TileState.CIRCLE &&
+                board[1][1] == TileState.CIRCLE && board[2][0] == TileState.CIRCLE))    {
+            return GameState.PLAYER_TWO;
+        }
+        for(int i=0; i<BOARD_SIZE; i++)
+            if((board[i][0] == TileState.CIRCLE && board[i][1] == TileState.CIRCLE &&
+                    board[i][2] == TileState.CIRCLE) || (board[0][i] == TileState.CIRCLE &&
+                    board[1][i] == TileState.CIRCLE && board[2][i] == TileState.CIRCLE))    {
+                return GameState.PLAYER_TWO;
+            }
+        for(int i=0; i<BOARD_SIZE; i++)
+            for(int j=0; j<BOARD_SIZE; j++)
+                if(board[i][j] == TileState.BLANK)  {
+                    return GameState.IN_PROGRESS;
+                }
+        return GameState.DRAW;
     }
 }
--- app/src/main/java/com/example/johan/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/johan/tic_tac_toe/MainActivity.java
@@ -7,15 +7,51 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static com.example.johan.tic_tac_toe.GameState.DRAW;
+import static com.example.johan.tic_tac_toe.GameState.PLAYER_TWO;
+
 public class MainActivity extends AppCompatActivity {
 
+    // Initialise game and arrays to loop through later on
     Game game;
+    int tiles[] = {R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5,
+            R.id.button6, R.id.button7, R.id.button8, R.id.button9};
+    String tilenames[] = {"button1", "button2", "button3", "button4", "button5", "button6",
+            "button7", "button8", "button9"};
+
+    // save text in tiles, tilestates and button enabled states
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState); // always call super
+        for (int i = 0; i < tiles.length; i++) {
+            outState.putCharSequence(tilenames[i], ((TextView) findViewById(tiles[i])).getText());
+        }
+        outState.putString("text", ((TextView) findViewById(R.id.textView3)).getText().toString());
+        outState.putSerializable("Buttons", game);
+        outState.putBoolean("Enabled", findViewById(tiles[0]).isEnabled());
+    }
+
+    // restore text in tiles, tilestates and button enabled states
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        game = new Game();
+        if (savedInstanceState != null) {
+            for (int i = 0; i < tiles.length; i++) {
+                ((TextView) findViewById(tiles[i])).setText(savedInstanceState.getCharSequence(tilenames[i]));
+            }
+            ((TextView) findViewById(R.id.textView3)).setText(savedInstanceState.getString("text"));
+            game  = (Game) savedInstanceState.getSerializable("Buttons");
+            Boolean Enabled = savedInstanceState.getBoolean("Enabled");
+            if(!Enabled) {
+                disableButtons();
+            }
+        }   else {
+            game = new Game();
+        }
     }
+
+    // check which tile is clicked and by which player
     public void tileClicked(View view) {
         Log.d("tictactoe", "tileClicked: " + String.valueOf(view.getId()));
         int id = view.getId();
@@ -49,31 +85,62 @@ public class MainActivity extends AppCompatActivity {
                 state = game.choose(2, 2);
                 break;
         }
+
+        // set text in clicked tile to right character
         switch(state) {
             case CROSS:
-                Button cross_button = (Button)findViewById(id);
+                Button cross_button = findViewById(id);
                 cross_button.setText("X");
-                // do something
                 break;
             case CIRCLE:
-                Button circle_button = (Button)findViewById(id);
+                Button circle_button = findViewById(id);
                 circle_button.setText("O");
-                // do something
                 break;
             case INVALID:
-                // do something different
                 break;
         }
-    }
-    public void resetClicked()  {
-        int [] tiles = {R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5,
-                        R.id.button6, R.id.button7, R.id.button8, R.id.button9}
-        for (int i = 0; i < 9; i++) {
-            Button tile = (Button) findViewById(getResources().getIdentifier("button" + i,
-                    "id", this.getPackageName()));
-            tile.setText(" ");
-            // do something
+
+        // check progress of the game and let players know the outcome
+        GameState won = game.won();
+        switch(won){
+            case IN_PROGRESS:
+                break;
+            case PLAYER_ONE:
+                TextView player_one = findViewById(R.id.textView3);
+                player_one.setText("Player 1 wins");
+                disableButtons();
+                break;
+            case PLAYER_TWO:
+                TextView player_two = findViewById(R.id.textView3);
+                player_two.setText("Player 2 wins");
+                disableButtons();
+                break;
+            case DRAW:
+                TextView draw = findViewById(R.id.textView3);
+                draw.setText("Draw");
+                disableButtons();
+                break;
+
         }
+    }
+
+    // disable all buttons
+    public void disableButtons()    {
+        for (int i = 0; i < tiles.length; i++) {
+            Button disable_button = findViewById(tiles[i]);
+            disable_button.setEnabled(false);
+        }
+    }
+
+    // set up UI to start new game
+    public void resetClicked(View view)  {
         game = new Game();
+        TextView draw = findViewById(R.id.textView3);
+        draw.setText("");
+        for (int i = 0; i < tiles.length; i++) {
+            Button clear_button = findViewById(tiles[i]);
+            clear_button.setText(" ");
+            clear_button.setEnabled(true);
+        }
     }
 }
